0f0bac60ca178ae8cf141ebcea28114fb43bf5fe
LojananS <126728354+LojananS@users.noreply.github.com>
2024-05-08 18:32:30 +1200

    Testing mouse input
    - Checking if mouse works

---
+++ QuartusTutorialeg/output_files/flappybird21.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(5CEBA4F23) Path("C:/Users/lsiv157/OneDrive - The University of Auckland/Documents/305-team-21/QuartusTutorialeg/output_files/") File("flappybird21.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- bouncy_ball.vhd
+++ bouncy_ball.vhd
@@ -6,7 +6,7 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 ENTITY bouncy_ball IS
 	PORT
-		( pb1, pb2, clk, vert_sync	: IN std_logic;
+		( pb1, pb2, clk, vert_sync, left_click	: IN std_logic;
           pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
 		  red, green, blue 			: OUT std_logic);		
 END bouncy_ball;
@@ -32,13 +32,14 @@ ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' &
 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
-Red <=  pb1;
+Red <=  pb1 and (pb2);
 Green <= (not pb2) and (not ball_on);
-Blue <=  not ball_on;
+Blue <=  (not pb2) and (not ball_on);
 
 
 Move_Ball: process (vert_sync)  	
 begin
+	
 	-- Move ball once every vertical sync
 	if (rising_edge(vert_sync)) then			
 		-- Bounce off top or bottom of the screen
@@ -46,6 +47,10 @@ begin
 			ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
 		elsif (ball_y_pos <= size) then 
 			ball_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+		end if;
+		
+		if (left_click = '1') then
+			ball_y_motion <= - CONV_STD_LOGIC_VECTOR(5,10);
 		end if;
 		-- Compute next ball Y position
 		ball_y_pos <= ball_y_pos + ball_y_motion;
