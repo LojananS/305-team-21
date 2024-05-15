c4ef24a558f2afe6196afa6c88340129e08b1606
Tony Huang <qhua835@UoA.auckland.ac.nz>
2024-05-15 18:24:33 +1200

    Added ground and make the block diagram tidier

--- code/QuartusTutorialeg/display_controller.vhd
+++ code/QuartusTutorialeg/display_controller.vhd
@@ -4,8 +4,8 @@ USE  IEEE.NUMERIC_STD.all;
 
 ENTITY display_controller IS
 	PORT
-		( clk, ball_on, pipe_on, background_on, text_on: IN std_logic;
-			RGB_ball, RGB_pipe, RGB_background	: IN std_logic_vector(11 downto 0);
+		( ball_on, pipe_on, background_on, text_on, ground_on: IN std_logic;
+			RGB_ball, RGB_pipe, RGB_background, RGB_ground	: IN std_logic_vector(11 downto 0);
 		  red, green, blue 			: OUT std_logic_vector(3 downto 0));		
 END display_controller;
 
@@ -23,6 +23,10 @@ begin
 		red <= RGB_ball(11 downto 8);
 		green <= RGB_ball(7 downto 4);
 		blue <= RGB_ball(3 downto 0);
+	elsif ground_on = '1' then
+		red <= RGB_ground(11 downto 8);
+		green <= RGB_ground(7 downto 4);
+		blue <= RGB_ground(3 downto 0);
 	elsif pipe_on = '1' then
 		red <= RGB_pipe(11 downto 8);
 		green <= RGB_pipe(7 downto 4);
