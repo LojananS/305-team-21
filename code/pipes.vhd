9a1abc109ef2efc4941e08da03e9a73540d4ab46
LojananS <126728354+LojananS@users.noreply.github.com>
2024-05-17 14:17:35 +1200

    Revert "Pipes still not added"
    This reverts commit d699ca7665c7f16a7ccfaafa1f51a7b1a38ce27d.

--- code/pipes.vhd
+++ code/pipes.vhd
@@ -10,30 +10,27 @@ ENTITY pipes IS
         output_on : OUT std_logic;
         RGB : OUT std_logic_vector(11 DOWNTO 0);
         p1_x_pos, p2_x_pos, p3_x_pos : OUT signed(10 DOWNTO 0);
-        p1_y_pos, p2_y_pos, p3_y_pos : OUT signed(9 DOWNTO 0)
+        p1_gap_center, p2_gap_center, p3_gap_center : OUT signed(9 DOWNTO 0)
     );		
 END pipes;
 
 ARCHITECTURE behavior OF pipes IS
     SIGNAL p1_on : std_logic;
-    SIGNAL p1_x_pos_internal : signed(10 DOWNTO 0) := to_signed(213, 11);
-    SIGNAL p1_y_pos_internal : signed(9 DOWNTO 0) := to_signed(240, 10);
+    SIGNAL p1_x_pos_internal : signed(10 DOWNTO 0) := to_signed(213, 11); 
+    SIGNAL p1_gap_center_internal : signed(9 DOWNTO 0) := to_signed(240, 10);
 
     SIGNAL p2_on : std_logic;
     SIGNAL p2_x_pos_internal : signed(10 DOWNTO 0) := to_signed(426, 11);
-    SIGNAL p2_y_pos_internal : signed(9 DOWNTO 0) := to_signed(360, 10);
+    SIGNAL p2_gap_center_internal : signed(9 DOWNTO 0) := to_signed(360, 10);
 
     SIGNAL p3_on : std_logic;
     SIGNAL p3_x_pos_internal : signed(10 DOWNTO 0) := to_signed(640, 11);
-    SIGNAL p3_y_pos_internal : signed(9 DOWNTO 0) := to_signed(100, 10);
+    SIGNAL p3_gap_center_internal : signed(9 DOWNTO 0) := to_signed(100, 10);
 
-    SIGNAL pipe_x_size : signed(9 DOWNTO 0) := to_signed(30, 10); -- Updated to 30 pixels width
+    SIGNAL pipe_x_size : signed(9 DOWNTO 0) := to_signed(30, 10);
     SIGNAL start_move : std_logic := '0';
 
     SIGNAL random_value : std_logic_vector(9 DOWNTO 0);
-	 
-    SIGNAL pipe_address : std_logic_vector(15 DOWNTO 0);
-    SIGNAL pipe_data : std_logic_vector(12 DOWNTO 0);
 
     COMPONENT galois_lfsr
         PORT
@@ -43,15 +40,6 @@ ARCHITECTURE behavior OF pipes IS
             random_value : OUT std_logic_vector(9 DOWNTO 0)
         );
     END COMPONENT;
-	 
-    COMPONENT pipe_rom IS
-    PORT
-    (
-        clk             :   IN STD_LOGIC;
-        pipe_address    :   IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        data_out        :   OUT STD_LOGIC_VECTOR(12 DOWNTO 0) -- 13 bits: 12-bit RGB + 1-bit transparency
-    );
-    END COMPONENT pipe_rom;
 
 BEGIN
     lfsr_inst: galois_lfsr
@@ -61,70 +49,28 @@ BEGIN
             random_value => random_value
         );
 
-    pipe_rom_inst : pipe_rom
-        PORT MAP (
-            clk => clk,
-            pipe_address => pipe_address,
-            data_out => pipe_data
-        );
+    p1_on <= '1' WHEN (p1_x_pos_internal + pipe_x_size > to_signed(0, 11) AND
+                       to_integer(unsigned(pixel_column)) >= to_integer(p1_x_pos_internal) AND 
+                       to_integer(unsigned(pixel_column)) < to_integer(p1_x_pos_internal) + to_integer(pipe_x_size) AND
+                       (to_integer(unsigned(pixel_row)) < to_integer(p1_gap_center_internal) - 45 OR 
+                        to_integer(unsigned(pixel_row)) > to_integer(p1_gap_center_internal) + 45))
+                ELSE '0';
 
-    Pipe_Display : PROCESS (clk)
-    BEGIN
-        IF rising_edge(clk) THEN
-            -- Check for pipe 1
-            IF (pixel_column >= std_logic_vector(signed(p1_x_pos_internal)) AND 
-                pixel_column < std_logic_vector(signed(p1_x_pos_internal) + pipe_x_size) AND
-                pixel_row >= std_logic_vector(signed(p1_y_pos_internal)) AND
-                pixel_row < std_logic_vector(signed(p1_y_pos_internal) + to_signed(240, 10))) THEN
-                pipe_address <= std_logic_vector(to_unsigned(
-                    (to_integer(signed(pixel_row)) - to_integer(p1_y_pos_internal)) * 30 + 
-                    (to_integer(signed(pixel_column)) - to_integer(p1_x_pos_internal)), 16));
-                IF pipe_data(0) = '1' THEN
-                    p1_on <= '1';
-                ELSE
-                    p1_on <= '0';
-                END IF;
-            ELSE
-                p1_on <= '0';
-            END IF;
+    p2_on <= '1' WHEN (p2_x_pos_internal + pipe_x_size > to_signed(0, 11) AND
+                       to_integer(unsigned(pixel_column)) >= to_integer(p2_x_pos_internal) AND 
+                       to_integer(unsigned(pixel_column)) < to_integer(p2_x_pos_internal) + to_integer(pipe_x_size) AND
+                       (to_integer(unsigned(pixel_row)) < to_integer(p2_gap_center_internal) - 45 OR 
+                        to_integer(unsigned(pixel_row)) > to_integer(p2_gap_center_internal) + 45))
+                ELSE '0';
 
-            -- Check for pipe 2
-            IF (pixel_column >= std_logic_vector(signed(p2_x_pos_internal)) AND 
-                pixel_column < std_logic_vector(signed(p2_x_pos_internal) + pipe_x_size) AND
-                pixel_row >= std_logic_vector(signed(p2_y_pos_internal)) AND
-                pixel_row < std_logic_vector(signed(p2_y_pos_internal) + to_signed(240, 10))) THEN
-                pipe_address <= std_logic_vector(to_unsigned(
-                    (to_integer(signed(pixel_row)) - to_integer(p2_y_pos_internal)) * 60 + 
-                    (to_integer(signed(pixel_column)) - to_integer(p2_x_pos_internal)), 16));
-                IF pipe_data(0) = '1' THEN
-                    p2_on <= '1';
-                ELSE
-                    p2_on <= '0';
-                END IF;
-            ELSE
-                p2_on <= '0';
-            END IF;
+    p3_on <= '1' WHEN (p3_x_pos_internal + pipe_x_size > to_signed(0, 11) AND
+                       to_integer(unsigned(pixel_column)) >= to_integer(p3_x_pos_internal) AND 
+                       to_integer(unsigned(pixel_column)) < to_integer(p3_x_pos_internal) + to_integer(pipe_x_size) AND
+                       (to_integer(unsigned(pixel_row)) < to_integer(p3_gap_center_internal) - 45 OR 
+                        to_integer(unsigned(pixel_row)) > to_integer(p3_gap_center_internal) + 45))
+                ELSE '0';
 
-            -- Check for pipe 3
-            IF (pixel_column >= std_logic_vector(signed(p3_x_pos_internal)) AND 
-                pixel_column < std_logic_vector(signed(p3_x_pos_internal) + pipe_x_size) AND
-                pixel_row >= std_logic_vector(signed(p3_y_pos_internal)) AND
-                pixel_row < std_logic_vector(signed(p3_y_pos_internal) + to_signed(240, 10))) THEN
-                pipe_address <= std_logic_vector(to_unsigned(
-                    (to_integer(signed(pixel_row)) - to_integer(p3_y_pos_internal)) * 120 + 
-                    (to_integer(signed(pixel_column)) - to_integer(p3_x_pos_internal)), 16));
-                IF pipe_data(0) = '1' THEN
-                    p3_on <= '1';
-                ELSE
-                    p3_on <= '0';
-                END IF;
-            ELSE
-                p3_on <= '0';
-            END IF;
-        END IF;
-    END PROCESS Pipe_Display;
-
-    RGB <= pipe_data(12 DOWNTO 1) WHEN (p1_on = '1' OR p2_on = '1' OR p3_on = '1');
+    RGB <= "100010001000" WHEN (p1_on = '1' OR p2_on = '1' OR p3_on = '1');
     output_on <= '1' WHEN (p1_on = '1' OR p2_on = '1' OR p3_on = '1') ELSE '0';
 
     Move_pipe: PROCESS (vert_sync, left_click, reset, collision, reset_pipes)
@@ -134,14 +80,14 @@ BEGIN
                 start_move <= '1';
             END IF;
 
-            IF reset_pipes = '1' AND start_move = '1' THEN
+            IF reset_pipes = '1' THEN
                 -- Reset pipes to their original positions
                 p1_x_pos_internal <= to_signed(213, 11);
-                p1_y_pos_internal <= to_signed(240, 10);
+                p1_gap_center_internal <= to_signed(240, 10);
                 p2_x_pos_internal <= to_signed(426, 11);
-                p2_y_pos_internal <= to_signed(360, 10);
+                p2_gap_center_internal <= to_signed(360, 10);
                 p3_x_pos_internal <= to_signed(640, 11);
-                p3_y_pos_internal <= to_signed(100, 10);
+                p3_gap_center_internal <= to_signed(100, 10);
                 start_move <= '1'; -- Restart pipes movement
             ELSIF start_move = '1' THEN
                 IF collision = '1' THEN
@@ -149,21 +95,21 @@ BEGIN
                 ELSE
                     IF (p1_x_pos_internal + pipe_x_size <= to_signed(0, 11)) THEN
                         p1_x_pos_internal <= to_signed(640, 11);
-                        p1_y_pos_internal <= (signed(random_value) MOD to_signed(310, 10)) + to_signed(55, 10);
+                        p1_gap_center_internal <= (signed(random_value) MOD to_signed(310, 10)) + to_signed(55, 10);
                     ELSE
                         p1_x_pos_internal <= p1_x_pos_internal - to_signed(1, 11);
                     END IF;
 
                     IF (p2_x_pos_internal + pipe_x_size <= to_signed(0, 11)) THEN
                         p2_x_pos_internal <= to_signed(640, 11);
-                        p2_y_pos_internal <= (signed(random_value) MOD to_signed(310, 10)) + to_signed(55, 10);
+                        p2_gap_center_internal <= (signed(random_value) MOD to_signed(310, 10)) + to_signed(55, 10);
                     ELSE
                         p2_x_pos_internal <= p2_x_pos_internal - to_signed(1, 11);
                     END IF;
 
                     IF (p3_x_pos_internal + pipe_x_size <= to_signed(0, 11)) THEN
                         p3_x_pos_internal <= to_signed(640, 11);
-                        p3_y_pos_internal <= (signed(random_value) MOD to_signed(310, 10)) + to_signed(55, 10);
+                        p3_gap_center_internal <= (signed(random_value) MOD to_signed(310, 10)) + to_signed(55, 10);
                     ELSE
                         p3_x_pos_internal <= p3_x_pos_internal - to_signed(1, 11);
                     END IF;
@@ -173,10 +119,10 @@ BEGIN
     END PROCESS Move_pipe;
 
     p1_x_pos <= p1_x_pos_internal;
-    p1_y_pos <= p1_y_pos_internal;
+    p1_gap_center <= p1_gap_center_internal;
     p2_x_pos <= p2_x_pos_internal;
-    p2_y_pos <= p2_y_pos_internal;
+    p2_gap_center <= p2_gap_center_internal;
     p3_x_pos <= p3_x_pos_internal;
-    p3_y_pos <= p3_y_pos_internal;
+    p3_gap_center <= p3_gap_center_internal;
 
 END behavior;
